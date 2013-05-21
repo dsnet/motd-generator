8247fd28117c7d83d8de8b3ae08509c4b26a304f
Joe Tsai <joetsai@digital-static.net>
2013-05-21 02:47:04 -0700

    Added exception clauses to handle parsing failures. Got memory and disk usage in raw bytes. Created function to convert bytes to human-readable format. Sanity check on process counts.

--- motd_gen.py
+++ motd_gen.py
@@ -15,81 +15,128 @@ def exec_cmd(cmd):
     lines = os.popen(cmd + ' 2> /dev/null', 'r').readlines()
     return [line.rstrip() for line in lines]
 
+def byte_unit(bytes):
+    units = ['B','KB','MB','GB','TB','PB','EB','ZB','YB']
+    value = bytes
+    for order in range(len(units)):
+        if round(value) < 1000:
+            return "%d %s" % (round(value),units[order])
+        value = value/1000.0
+
 info_list = []
 
 # Get last login
 login = exec_cmd('lastlog -u $USER')[-1].strip()
-user,port,host,last = login.split(None,3)
-info_list.append(('Last login:', '%s from %s' % (last,host)))
+try:
+    user,port,host,last = login.split(None,3)
+    info_list.append(('Last login:', '%s from %s' % (last,host)))
+except:
+    info_list.append(('Last login:', ' '.join(login.split())))
 
 # Get uptime
-uptime = ''.join(exec_cmd('cat /proc/uptime')).strip()
-total_time,idle_time = [int(float(x)) for x in uptime.split()]
-days = total_time/60/60/24
-hours = total_time/60/60%24
-minutes = total_time/60%60
-seconds = total_time%60
+uptime = ''.join(exec_cmd('/bin/cat /proc/uptime')).strip()
+try:
+    total_time,idle_time = [int(float(x)) for x in uptime.split()]
+    days = total_time/60/60/24
+    hours = total_time/60/60%24
+    minutes = total_time/60%60
+    seconds = total_time%60
 
-message_list = []
-message_list.append('%s days' % days)
-message_list.append('%s hours' % hours)
-message_list.append('%s minutes' % minutes)
-message_list.append('%s seconds' % seconds)
-info_list.append(('Uptime:', ', '.join(message_list)))
+    message_list = []
+    message_list.append('%s days' % days)
+    message_list.append('%s hours' % hours)
+    message_list.append('%s minutes' % minutes)
+    message_list.append('%s seconds' % seconds)
+
+    if message_list:
+        info_list.append(('Uptime:', ', '.join(message_list)))
+except:
+    info_list.append(('Uptime:', uptime))
 
 # Get CPU information
-cpu_info = exec_cmd('cat /proc/cpuinfo')
+cpu_info = exec_cmd('/bin/cat /proc/cpuinfo')
+try:
+    # Get model
+    model = ''
+    for line in cpu_info:
+        line_sub = re.sub(r'^model name\s*:','',line)
+        if line != line_sub:
+            model = re.sub(r'\([rR]\)|\([tT][mM]\)','',line_sub)
+            model = ' '.join(model.strip().split())
+            break
 
-model = ''
-for line in cpu_info:
-    line_sub = re.sub(r'^model name\s*:','',line)
-    if line != line_sub:
-        model = re.sub(r'\([rR]\)|\([tT][mM]\)','',line_sub)
-        model = ' '.join(model.strip().split())
-        break
-cores = 0
-for line in cpu_info:
-    if re.match(r'^processor\s*:',line):
-        cores += 1
-message = '%s, %sx cores' % (model,cores)
-info_list.append(('CPU info:', message))
+    # Get cores
+    cores = 0
+    for line in cpu_info:
+        if re.match(r'^processor\s*:',line):
+            cores += 1
+    cores = "%sx cores" % cores
+
+    if model and cores:
+        message = '%s, %s' % (model,cores)
+        info_list.append(('CPU info:', message))
+except:
+    pass
 
 # Get CPU load
-cpu_load = ''.join(exec_cmd('cat /proc/loadavg')).strip()
-loads = [float(x) for x in cpu_load.split(None,3)[:3]]
-loads_text = []
-for load in loads:
-    percent_text = '%.2f%%' % load
-    loads_text.append(percent_text)
-values = tuple(loads_text)
-message = "%s (1 minute) - %s (5 minutes) - %s (15 minutes)" % values
-info_list.append(('CPU load:', message))
+cpu_load = ''.join(exec_cmd('/bin/cat /proc/loadavg')).strip()
+try:
+    loads = [float(x) for x in cpu_load.split(None,3)[:3]]
+    loads_text = []
+    for load in loads:
+        percent_text = '%.2f%%' % load
+        loads_text.append(percent_text)
+    values = tuple(loads_text)
+    message = "%s (1 minute) - %s (5 minutes) - %s (15 minutes)" % values
+    info_list.append(('CPU load:', message))
+except:
+    pass
 
 # Get memory usage
-mem_usage = exec_cmd('free -m')
-label,total,used,free,others = mem_usage[1].split(None,4)
-total,used,free = int(total),int(used),int(free)
-percent = '%.2f%%' % ((float(used)/float(total))*100.0)
-values = percent,total,used,free
-message = "%s - %sMB total, %sMB used, %sMB free" % values
-info_list.append(('Memory usage:', message))
+mem_usage = exec_cmd('free -b')
+try:
+    mem_line = ''
+    for line in mem_usage:
+        if re.search(r'^Mem:',line):
+            mem_line = line.strip()
+            break
+    if mem_line:
+        label,total,used,free,others = mem_line.split(None,4)
+        total,used,free = int(total),int(used),int(free)
+        percent = (float(used)/float(total))*100.0
+        percent_text = '%.2f%%' % percent
+        values = percent_text,byte_unit(total),byte_unit(used),byte_unit(free)
+        message = "%s - %s total, %s used, %s free" % values
+        info_list.append(('Memory usage:', message))
+except:
+    pass
 
 # Get disk usage
-disk_usage = exec_cmd('df -h /')[-1].strip()
-label,total,used,free,percent,others = disk_usage.split(None,5)
-values = percent,total,used,free
-message = "%s - %sB total, %sB used, %sB free" % values
-info_list.append(('Disk usage:', message))
+disk_usage = exec_cmd('df -B 1 /')[-1].strip()
+try:
+    label,blocks,used,free,others = disk_usage.split(None,4)
+    total,used,free = int(used)+int(free),int(used),int(free)
+    percent = (float(used)/float(total))*100.0
+    percent_text = '%.2f%%' % percent
+    values = percent_text,byte_unit(total),byte_unit(used),byte_unit(free)
+    message = "%s - %s total, %s used, %s free" % values
+    info_list.append(('Disk usage:', message))
+except:
+    pass
 
 # Get processes
 user_procs = len(exec_cmd('ps U $USER h'))
 total_procs = len(exec_cmd('ps -A h'))
-values = user_procs,total_procs
-message = "Running %s processes out of %s total" % values
-info_list.append(('Processes:', message))
+try:
+    if (total_procs or user_procs) and (total_procs >= user_procs):
+        values = user_procs,total_procs
+        message = "Running %s processes out of %s total" % values
+        info_list.append(('Processes:', message))
+except:
+    pass
 
 # Print the welcome message
-os_issue = ''.join(exec_cmd('cat /etc/issue')).strip()
+os_issue = ''.join(exec_cmd('/bin/cat /etc/issue')).strip()
 os_name = re.sub(r'\\[a-zA-Z]','',os_issue).strip()
 full_name = ''.join(exec_cmd('domainname -f')).strip()
 print " Welcome to %s running %s" % (full_name,os_name)
